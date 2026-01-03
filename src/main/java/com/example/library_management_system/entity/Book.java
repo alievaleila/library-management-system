1112ede1cf2dfcb1d23fac2ce5a6c08e30206e91
alievaleila <meliyeva452@gmail.com>
2026-01-03 23:33:51 +0400

    feat: implement Category and Book entities with bidirectional relationship

---
+++ src/main/java/com/example/library_management_system/entity/Book.java
@@ -0,0 +1,33 @@
+package com.example.library_management_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
