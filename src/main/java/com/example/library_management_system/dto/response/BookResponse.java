ae828f88ad0287282062a524cdde635c270fcf5e
alievaleila <meliyeva452@gmail.com>
2026-01-04 22:39:32 +0400

    feat: created BookResponse to structure outgoing book information

--- src/main/java/com/example/library_management_system/dto/response/BookResponse.java
+++ src/main/java/com/example/library_management_system/dto/response/BookResponse.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class BookResponse {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String categoryName;
 }
