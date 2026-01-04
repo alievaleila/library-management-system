2450f932a9d26dea4452307a3751af8b5c4feacc
alievaleila <meliyeva452@gmail.com>
2026-01-04 22:58:35 +0400

    feat: complete Book module integration with Service

--- src/main/java/com/example/library_management_system/service/BookService.java
+++ src/main/java/com/example/library_management_system/service/BookService.java
@@ -1,4 +1,15 @@
 package com.example.library_management_system.service;
 
+import com.example.library_management_system.dto.request.BookRequest;
+import com.example.library_management_system.dto.response.BookResponse;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
 public interface BookService {
+
+    @Nullable
+    List<BookResponse> getAll();
+
+    BookResponse save(BookRequest request);
 }
--- src/main/java/com/example/library_management_system/service/impls/BookServiceImpl.java
+++ src/main/java/com/example/library_management_system/service/impls/BookServiceImpl.java
@@ -1,10 +1,25 @@
 package com.example.library_management_system.service.impls;
 
+import com.example.library_management_system.dto.request.BookRequest;
+import com.example.library_management_system.dto.response.BookResponse;
 import com.example.library_management_system.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
+
+    @Override
+    public @Nullable List<BookResponse> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public BookResponse save(BookRequest request) {
+        return null;
+    }
 }
