4f151c921115e1c33b07a3ccd40aa9edea96042c
alievaleila <meliyeva452@gmail.com>
2026-01-04 22:51:08 +0400

    feat: implement Book module with full REST integration

--- src/main/java/com/example/library_management_system/controller/BookController.java
+++ src/main/java/com/example/library_management_system/controller/BookController.java
@@ -1,9 +1,19 @@
 package com.example.library_management_system.controller;
 
+import com.example.library_management_system.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
+
+
+
 
 
 }
