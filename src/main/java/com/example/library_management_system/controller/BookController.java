03e22a641ed9ba5372be586dc6c958cecea21340
alievaleila <meliyeva452@gmail.com>
2026-01-04 22:56:14 +0400

    feat: structured API response using ResponseEntity and HttpStatus

--- src/main/java/com/example/library_management_system/controller/BookController.java
+++ src/main/java/com/example/library_management_system/controller/BookController.java
@@ -1,9 +1,18 @@
 package com.example.library_management_system.controller;
 
+import com.example.library_management_system.dto.request.BookRequest;
+import com.example.library_management_system.dto.response.BookResponse;
 import com.example.library_management_system.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/books")
@@ -12,8 +21,13 @@ public class BookController {
 
     private final BookService bookService;
 
+    @GetMapping
+    public ResponseEntity<List<BookResponse>> getAll() {
+        return ResponseEntity.ok(bookService.getAll());
+    }
 
-
-
-
+    @PostMapping
+    public ResponseEntity<BookResponse> create(@RequestBody BookRequest request) {
+        return new ResponseEntity<>(bookService.save(request), HttpStatus.CREATED);
+    }
 }
