dd2a016d40b4ea1047a362b13c44bffc4584b7a3
alievaleila <meliyeva452@gmail.com>
2026-01-04 21:12:43 +0400

    feat: implement Category service with MapStruct and Stream API

--- src/main/java/com/example/library_management_system/service/impls/CategoryServiceImpl.java
+++ src/main/java/com/example/library_management_system/service/impls/CategoryServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.library_management_system.service.impls;
 
 import com.example.library_management_system.dto.request.CategoryRequest;
 import com.example.library_management_system.dto.response.CategoryResponse;
+import com.example.library_management_system.entity.Category;
+import com.example.library_management_system.mapper.CategoryMapper;
 import com.example.library_management_system.repository.CategoryRepository;
 import com.example.library_management_system.service.CategoryService;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +16,18 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
     @Override
     public List<CategoryResponse> getAll() {
-
+        List<Category>categories=categoryRepository.findAll();
+        return categories.stream().map(categoryMapper::toResponse).toList();
     }
 
     @Override
     public CategoryResponse save(CategoryRequest request) {
-
+        Category category=categoryMapper.toEntity(request);
+        Category savedCategory=categoryRepository.save(category);
+        return categoryMapper.toResponse(savedCategory);
     }
 }
