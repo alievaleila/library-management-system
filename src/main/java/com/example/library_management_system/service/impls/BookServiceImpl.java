bc5e2a3533a4e832b8bc6ad02d2c44f69db7c906
alievaleila <meliyeva452@gmail.com>
2026-01-04 18:52:42 +0400

    feat: scaffold web and service layers for Category and Book management

---
+++ src/main/java/com/example/library_management_system/service/impls/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.library_management_system.service.impls;
+
+import com.example.library_management_system.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+}
---
+++ src/main/java/com/example/library_management_system/service/impls/CategoryServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.library_management_system.service.impls;
+
+import com.example.library_management_system.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+}
