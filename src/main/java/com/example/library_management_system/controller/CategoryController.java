b5e07fc82f5e396b9e0b4966b74e564293375849
alievaleila <meliyeva452@gmail.com>
2026-01-05 23:15:43 +0400

    feat(controller): add getById endpoint for Book

--- src/main/java/com/example/library_management_system/controller/CategoryController.java
+++ src/main/java/com/example/library_management_system/controller/CategoryController.java
@@ -1,12 +1,15 @@
 package com.example.library_management_system.controller;
 
 import com.example.library_management_system.dto.request.CategoryRequest;
+import com.example.library_management_system.dto.response.BookResponse;
 import com.example.library_management_system.dto.response.CategoryResponse;
+import com.example.library_management_system.service.BookService;
 import com.example.library_management_system.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,7 @@ import java.util.List;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final BookService bookService;
 
     @GetMapping
     public ResponseEntity<List<CategoryResponse>> getAll() {
@@ -31,4 +35,11 @@ public class CategoryController {
     public ResponseEntity<CategoryResponse> create(@RequestBody CategoryRequest request) {
         return new ResponseEntity<>(categoryService.save(request), HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getById(id));
+    }
+
+
 }
